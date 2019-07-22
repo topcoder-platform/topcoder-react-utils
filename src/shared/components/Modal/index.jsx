2853eaf7eaf19c92fd19469cdeaf482de61dee66
gets0ul <gets0ul@gmail.com>
2019-07-22 19:56:52 +0700

    MSFT 248 - Add aria-label to close modal dialog button.

--- src/shared/components/Modal/index.jsx
+++ src/shared/components/Modal/index.jsx
@@ -54,6 +54,7 @@ class BaseModal extends React.Component {
             onClick={() => onCancel()}
             className={theme.overlay}
             type="button"
+            aria-label="Close Dialog"
           />
         </React.Fragment>
       ),
